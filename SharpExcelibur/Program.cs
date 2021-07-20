51ce74aed2d9a2b088217de862e7de3a3197d74d
SharpExcelibur <>
2021-07-20 00:52:37 -0400

    update
    Added Password protection checks and inline decryption of password protected spreadsheets :)

--- SharpExcelibur/Program.cs
+++ SharpExcelibur/Program.cs
@@ -11,7 +11,7 @@ namespace SharpExcelibur
     {
         static void Main(string[] args)
         {
-            if (args.Length < 1 || args.Length > 3)
+            if (args.Length < 1 || args.Length > 4)
             {
                 Console.WriteLine(@"
 
@@ -30,10 +30,12 @@ namespace SharpExcelibur
 "Developed By: @sadpanda_sec \n\n" +
 "Description: Read Contents of Excel Documents (XLS/XLSX).\n\n" +
 "Usage:\n" +
-"   List Worksheets: SharpExcelibur.exe sheets C:\\Some\\Path\\To\\ExcelWorkbook\n" +
-"   Read Sheet Data: SharpExcelibur.exe read <sheetname> C:\\Some\\Path\\To\\ExcelWorkbook\n\n" +
-"");
-                System.Environment.Exit(0);
+"Check if PW Protected:         SharpExcelibur.exe check C:\\Some\\Path\\To\\ExcelWorkbook\n" +
+"List Worksheets:               SharpExcelibur.exe sheets C:\\Some\\Path\\To\\ExcelWorkbook\n" +
+"List Protected Worksheets:     SharpExcelibur.exe sheets P@ssw0rd C:\\Some\\Path\\To\\ExcelWorkbook\n" +
+"Read Sheet Data:               SharpExcelibur.exe read <sheetname> C:\\Some\\Path\\To\\ExcelWorkbook\n" +
+"Read Protected Sheet Data:     SharpExcelibur.exe read <sheetname> P@ssw0rd C:\\Some\\Path\\To\\ExcelWorkbook\n");
+            System.Environment.Exit(0);
 
             }
             else if (args.Length == 2)
@@ -56,7 +58,7 @@ namespace SharpExcelibur
                                 i++;                               
                             }
 
-                            xlWorkbook.Close(true, null, null);
+                            xlWorkbook.Close(false, null, null);
                             xlApp.Quit();
                             Marshal.ReleaseComObject(xlWorkbook);
                             Marshal.ReleaseComObject(xlApp);
@@ -71,18 +73,100 @@ namespace SharpExcelibur
                     }
                   
                 }
+                else if (args[0].ToLower() == "check")
+                {
+                    if (File.Exists(Path.GetFullPath(args[1])))
+                    {
+                        if (Path.GetExtension(args[1]).ToLower() == ".xls" || Path.GetExtension(args[1]).ToLower() == ".xlsx")
+                        {
+                            try
+                            {
+                                Excel.Application xlApp = new Excel.Application();
+                                xlApp.Visible = false;
+                                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(Path.GetFullPath(args[1]),Type.Missing,true,Type.Missing," ");
+                                xlWorkbook.Close(false, null, null);
+                                xlApp.Quit();
+                                Marshal.ReleaseComObject(xlWorkbook);
+                                Marshal.ReleaseComObject(xlApp);
+                                Console.WriteLine("Excel Document NOT Password Protected");
+                                System.Environment.Exit(0);
+                            }
+                            catch (COMException ex)
+                            {
+                                Console.WriteLine("Excel Document is Password Protected");
+                                System.Environment.Exit(0);
+                            }
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error...Did not provide an Excel Worksheet");
+                            System.Environment.Exit(0);
+                        }
+
+                    }
+
+                }
                 else
                 {
                         Console.WriteLine("Error...Wrong arguments passed...See help menu");
                         Console.WriteLine($"Provided arguments: SharpExcelibur.exe" + " " + args[0] + " " + args[1]);
                         System.Environment.Exit(0);
-
                 }
 
             }
+
             else if (args.Length == 3) 
             {
-                if (args[0].ToLower() == "read")
+                if (args[0].ToLower() == "sheets")
+                {
+                    if (File.Exists(Path.GetFullPath(args[2])))
+                    {
+                        if (Path.GetExtension(args[2]).ToLower() == ".xls" || Path.GetExtension(args[2]).ToLower() == ".xlsx")
+                        {
+                            try
+                            {
+                                string pass = args[1].ToString();
+                                Excel.Application xlApp = new Excel.Application();
+                                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(Path.GetFullPath(args[2]), Type.Missing, true, Type.Missing, pass, pass);
+                                xlWorkbook.Password = pass;
+                                xlWorkbook.Unprotect(pass);
+                                String[] sheets = new string[xlWorkbook.Worksheets.Count];
+
+                                int i = 0;
+                                foreach (Excel.Worksheet wSheet in xlWorkbook.Worksheets)
+                                {
+                                    wSheet.Unprotect(pass);
+                                    Console.WriteLine("Sheet Name Found: " + (sheets[i] = wSheet.Name));
+                                    i++;
+                                }
+
+                                xlWorkbook.Close(false, null, null);
+                                xlApp.Quit();
+                                Marshal.ReleaseComObject(xlWorkbook);
+                                Marshal.ReleaseComObject(xlApp);
+                                System.Environment.Exit(0);
+                            }
+                            catch (COMException)
+                            {
+                                Console.WriteLine("Error: Supplied Incorrect Password");
+                                System.Environment.Exit(0);
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error...Did not provide an Excel Worksheet");
+                            System.Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error...Did not provide an Excel Worksheet");
+                        System.Environment.Exit(0);
+                    }
+                }
+                else if (args[0].ToLower() == "read")
                 {
                     if (File.Exists(Path.GetFullPath(args[2])))
                     {
@@ -138,10 +222,80 @@ namespace SharpExcelibur
 
                 }
             }
+            else if (args.Length == 4)
+            {
+                if (args[0].ToLower() == "read")
+                {
+                    if (File.Exists(Path.GetFullPath(args[3])))
+                    {
+                        if (Path.GetExtension(args[3]).ToLower() == ".xls" || Path.GetExtension(args[3]).ToLower() == ".xlsx")
+                        {
+                            try
+                            {
+                                string pass = args[2].ToString();
+                                Excel.Application xlApp = new Excel.Application();
+                                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(Path.GetFullPath(args[3]), Type.Missing, true, Type.Missing, pass, pass);
+                                xlWorkbook.Password = pass;
+                                xlWorkbook.Unprotect(pass);
+                                Excel._Worksheet xlWorksheet = (Excel._Worksheet)xlWorkbook.Sheets[args[1]];
+                                xlWorksheet.Unprotect(pass);
+                                Excel.Range xlRange = xlWorksheet.UsedRange;
+                                int rowCount = xlRange.Rows.Count;
+                                int colCount = xlRange.Columns.Count;
+
+                                for (int i = 1; i <= rowCount; i++)
+                                {
+                                    for (int j = 1; j <= colCount; j++)
+                                    {
+                                        //new line
+                                        if (j == 1)
+                                            Console.Write("\r\n");
+
+                                        //write the value to the console
+                                        if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Text != null)
+                                            Console.Write(xlRange.Cells[i, j].Text.ToString() + "\t");
+                                    }
+
+                                }
+                                xlWorkbook.Close(false, null, null);
+                                xlApp.Quit();
+                                Marshal.ReleaseComObject(xlWorksheet);
+                                Marshal.ReleaseComObject(xlWorkbook);
+                                Marshal.ReleaseComObject(xlApp);
+                                System.Environment.Exit(0);
+                            }
+                            catch (COMException)
+                            {
+                                Console.WriteLine("Error: Supplied Incorrect Password");
+                                System.Environment.Exit(0);
+                            }
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error...Did not provide path to Excel Worksheet");
+                            System.Environment.Exit(0);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error...Excel file doesnt exists");
+                        System.Environment.Exit(0);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error...Wrong arguments passed...See help menu");
+                    Console.WriteLine($"Provided arguments: SharpExcelibur.exe" + " " + args[0] + " " + args[1] + " " + args[2]);
+                    System.Environment.Exit(0);
+
+                }
+            }
             else
             {
                 Console.WriteLine("Error...Wrong arguments passed...See help menu");
-                Console.WriteLine($"Provided arguments: SharpExcelibur.exe" + " " + args[0] + " " + args[1] + " " + args[2]);
+                Console.WriteLine($"Provided arguments: SharpExcelibur.exe" + " " + args[0] + " " + args[1] + " " + args[2] + " " + args[3]);
                 System.Environment.Exit(0);
 
             }
@@ -149,5 +303,3 @@ namespace SharpExcelibur
         }
     }
 }
-
-
